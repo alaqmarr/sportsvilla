75e421bbb0f46805c42abb34bb1c96f931310ea8
Al Aqmar <alaqmarak0810@gmail.com>
2026-07-18 18:22:25 +0530

    type errors resolved

--- src/app/tournaments/TournamentFormClient.tsx
+++ src/app/tournaments/TournamentFormClient.tsx
@@ -83,7 +83,7 @@ export default function TournamentFormClient({ initialData, sports }: { initialD
       if (res.error) toast.error(res.error);
       else {
         toast.success(initialData?.id ? 'Tournament updated!' : 'Tournament created!');
-        if (!initialData?.id) {
+        if (!initialData?.id && res.tournament?.id) {
           router.push(`/tournaments/${res.tournament.id}`);
         } else {
           // just stay on page if editing
